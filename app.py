7bd3b8af3311f5faf9c91bf59741700a4049a881
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-12-08 14:08:26 -0500

    app db post

--- app.py
+++ app.py
@@ -5,19 +5,26 @@ from flask import Flask, json, request
 from bson.json_util import dumps
 
 # globals
+
 URI = "mongodb://heroku_z03b9kqp:eit8th40c96ss3e2mqlph3loo5@ds253418.mlab.com:53418/heroku_z03b9kqp"
 db_50    = MongoClient(URI)['concurrency']
 listings = db_50['listings']
 stocks   = db_50['stocks']
 users    = db_50['users']
 websites = db_50['websites']
-user = ['farmer', 'john', 'tomato']
+
 
 api = Flask(__name__)
 
 @api.route('/', methods=['GET'])
 def get_index():
 	return json.dumps('successfully set the app')
+
+@api.route('/add_user', methods=['POST'])
+def add_user():
+	req_data = request.get_json()
+	return json.dumps(req_date)
+
 
 @api.route('/users', methods=['GET'])
 def get_users():
