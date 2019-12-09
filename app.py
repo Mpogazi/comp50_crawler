1491e27e378dc6c705f2f11491e863162ca6a5ed
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-12-09 12:15:34 -0500

    adding sendgrid to requirements.txt

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def send_email():
 		subject='Sending with Twilio SendGrid is Fun',
 		html_content='<strong>and easy to do anywhere, even with Python</strong>')
 
-	sg = SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+	sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 	response = sg.send(message)
 	print(response.status_code, response.body, response.headers)
 
